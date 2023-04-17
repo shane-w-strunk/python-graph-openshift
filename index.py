896de123d04d0b471c24193d8d5b94233792c33d
shane-w-strunk <131016174+shane-w-strunk@users.noreply.github.com>
2023-04-17 14:21:04 -0400

    change file name

---
+++ index.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5, 6]
+y = [1, 5, 3, 5, 7, 8]
+
+plt.plot(x, y)
+plt.show()
+
+plt.savefig("output.jpg")
+print("succesfully built, shown, and saved graph as jpg")
